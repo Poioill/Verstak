d052966ac6183d422ce26804772c8e9e62a70a7a
Poioill <satkynovaz@gmail.com>
2023-07-05 03:05:50 +0600

    slider

---
+++ flex-practice-tutor/js/tennis.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.trainers__slider').slick({
+        slidesToShow: 6,
+        appendArrows: $('.trainers__slider-click'),
+        infinite: true,
+    })
+});
